25b883ef8d9dea789d792242e5812664f2f7120e
Oliver <doeixd@gmail.com>
2018-09-13 07:00:55 -0400

    all

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const cookieParser = require('cookie-parser')
 const app = express()
 const bodyParser = require('body-parser')
