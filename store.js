ae740e3fa146e8c77c0942ad7b16f7ff9af368c3
Oliver <oliver@localhost.shadow>
2018-09-29 11:48:31 -0400

    djusted vibration

--- store.js
+++ store.js
@@ -96,7 +96,7 @@ var store = observable({
 
   addTodo(todo) {
     this.todos.push(todo)
-    window.navigator.vibrate([100, 30, 20 ])
+    window.navigator.vibrate([100, 40, 30 ])
     this.updateStorage()
   },
 
